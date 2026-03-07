14b545c7703018419e0e82ef6d870ebb95444773
Spirit <kaveefernando67@gmail.com>
2026-03-08 05:27:49 +0530

    fix cors for frontend

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,17 @@ from predict import router as predict_router
 
 app = FastAPI()
 
+origins = [
+    "https://star-suitability-predictor-1.onrender.com",
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
